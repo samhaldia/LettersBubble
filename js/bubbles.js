b6c014dcc0c94361b0ddac31057f736000027dfb
Sameer <samhaldia123@gmail.com>
2018-03-22 02:34:00 +0530

    third commit

--- js/bubbles.js
+++ js/bubbles.js
@@ -156,8 +156,8 @@ function initEventListeners() {
  
 function updateCanvasDimensions() {
     canvas.attr({
-        height: 310,
-        width: 1050
+        height: 270,
+        width: 1000
     });
     canvasWidth = canvas.width();
     canvasHeight = canvas.height();
